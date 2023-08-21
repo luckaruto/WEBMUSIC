2a2595be9fbcf36c297d88f022a688e5398664b0
luckaruto <lucmten@gmail.com>
2023-08-21 15:05:03 +0700

    commit_giaodien1

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 
 import React from "react";
 import HomePage from "./Pages/Homepage.jsx";
+import View from "./Pages/View.jsx";
+import Slider from "./components/Slider.jsx";
+import PlayMusic from "./components/PlayMusic.jsx";
+import Optional from "./components/Optional.jsx";
+import { Sidebar } from "react-pro-sidebar";
 
 function App() {
   const Layout = () => {
     return (
       <>
-        {/* <Header />
-        <Outlet />
-        <ScrollButton />
-        <Footer /> */}
-        <Outlet />
+        {/* <Outlet />
+        <Sidebar />
+        <Optional />
+        <PlayMusic /> */}
       </>
     );
   };
@@ -20,9 +24,10 @@ function App() {
   const route = createBrowserRouter([
     {
       path: "/",
-      element: <Layout />,
+      // element: <Layout />,
       children: [
         { path: "/", element: <HomePage /> },
+        { path: "/view", element: <View /> },
         // {
         //   path: "/courses",
         //   element: <Courses />,
---
+++ src/components/View.jsx
@@ -0,0 +1,26 @@
+import { useSelector } from "react-redux";
+import { Route, Routes } from "react-router-dom";
+
+import PlayNow from "../components/PlayNow";
+
+const View = () => {
+  const { activeSong } = useSelector((state) => state.player);
+
+  return (
+    <div className="relative flex">
+      <div className="flex-1 flex flex-col ">
+        <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
+          <div className="flex-1 h-fit pb-40">
+            <PlayNow />
+          </div>
+        </div>
+      </div>
+
+      {activeSong?.title && (
+        <div className="absolute h-28 bottom-0 left-0 right-0 flex animate-slideup bg-gradient-to-br from-white/10 to-[#2a2a80] backdrop-blur-lg rounded-t-3xl z-10"></div>
+      )}
+    </div>
+  );
+};
+
+export default View;
